c5fb1a91f8139825e93d6368e505b3b5e56187df
Milton Maior <miltonvieirasoutomaior09@outlook.com>
2020-05-08 21:53:54 -0300

    testando a classe categoria

--- src/main/java/com/milton/spboot/resources/CategoriaResource.java
+++ src/main/java/com/milton/spboot/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.milton.spboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.milton.spboot.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String Listar() {
-		return "Rest funcionando";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }

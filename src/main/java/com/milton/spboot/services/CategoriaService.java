6299c1c6f4f04c5032d0b09ad9638070ae7bf54c
Milton Maior <miltonvieirasoutomaior09@outlook.com>
2020-05-10 17:43:13 -0300

    inserindo uma nova Categoria com post

--- src/main/java/com/milton/spboot/services/CategoriaService.java
+++ src/main/java/com/milton/spboot/services/CategoriaService.java
@@ -12,14 +12,16 @@ import com.milton.spboot.services.exception.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 	
-
 	@Autowired
 	private CategoriaRepository repo;
 	
-
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }

629cb5f12575fbf667dc4402bf1ac4c503752208
Milton Maior <miltonvieirasoutomaior09@outlook.com>
2020-05-20 21:04:06 -0300

    Busca de Produtos por nome e categoria

---
+++ src/main/java/com/milton/spboot/services/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.milton.spboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.milton.spboot.domain.Categoria;
+import com.milton.spboot.domain.Produto;
+import com.milton.spboot.repositories.CategoriaRepository;
+import com.milton.spboot.repositories.ProdutoRepository;
+import com.milton.spboot.services.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRep;
+	
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		List<Categoria> categorias = categoriaRep.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}	
+}

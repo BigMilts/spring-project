18de98f649799516b42d592973defd52bfb68149
Milton Maior <miltonvieirasoutomaior09@outlook.com>
2020-06-12 17:48:40 -0300

    Implementando autorizacao

--- src/main/java/com/milton/spboot/config/SecurityConfig.java
+++ src/main/java/com/milton/spboot/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.milton.spboot.security.JWTAuthenticationFilter;
+import com.milton.spboot.security.JWTAuthorization;
 import com.milton.spboot.security.JWTUtil;
 
 @Configuration
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorization(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
